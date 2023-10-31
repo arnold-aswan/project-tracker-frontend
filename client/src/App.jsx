6a74c95afb9c9c210e202b172066611070ab3424
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-10-31 11:39:52 +0300

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
 // src/App.jsx
 import React from "react";
-import AuthComponent from "./AuthComponent";
-import "./App.css"
+import { Route, Routes, Outlet } from "react-router-dom";
+import AuthComponent from "./components/AuthComponent";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddCohort from "./components/AddCohort";
+import AddProject from "./components/AddProject";
+import Cohorts from "./components/Cohorts";
+import Contact from "./components/ContactUs";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <AuthComponent />
+      <Navbar />
+      <Outlet />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cohorts" element={<Cohorts />} />
+        <Route path="/add-cohort" element={<AddCohort />} />
+        <Route path="/add-project" element={<AddProject />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/login/*" element={<AuthComponent />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
