6bc1ee8777cb7d0d5ecb38147409567b3cf5d4ba
samuel <samuel.mbula@student.moringaschool.com>
2023-11-03 09:35:48 +0300

    tests

--- client/src/MyComponent.test.js
+++ client/src/MyComponent.test.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AboutUs from './AboutUs';
+describe('AboutUs Component', () => {
+    it('renders the component', () => {
+      const { getByText } = render(<AboutUs />);
+      
