57e347ca1b6a1fe52abc4588071a379d6585c973
samuel <samuel.mbula@student.moringaschool.com>
2023-11-03 09:38:16 +0300

    tests

--- client/src/MyComponent.test.js
+++ client/src/MyComponent.test.js
@@ -7,3 +7,14 @@ describe('AboutUs Component', () => {
  // To est if the component renders without errors
  expect(getByText('About Us')).toBeInTheDocument();
 });
+it('displays mission and vision content', () => {
+    const { getByText } = render(<AboutUs />);
+
+    // Test that the mission and vision content is present
+    expect(getByText('Our Mission')).toBeInTheDocument();
+    expect(getByText('Our Vision')).toBeInTheDocument();
+
+    // You can add more specific content checks here
+    expect(getByText('We are on a mission to revolutionize the way teams manage their projects.')).toBeInTheDocument();
+    expect(getByText('We envision a world where every project, no matter the size or complexity, can be efficiently managed and completed on time.')).toBeInTheDocument();
+  });
