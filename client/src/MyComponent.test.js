6429b13db1d6f62079647cd7b7313caed37e9d91
samuel <samuel.mbula@student.moringaschool.com>
2023-11-03 09:37:01 +0300

    tests

--- client/src/MyComponent.test.js
+++ client/src/MyComponent.test.js
@@ -4,4 +4,6 @@ import AboutUs from './AboutUs';
 describe('AboutUs Component', () => {
     it('renders the component', () => {
       const { getByText } = render(<AboutUs />);
-      
+ // To est if the component renders without errors
+ expect(getByText('About Us')).toBeInTheDocument();
+});
